5f5385662496ede6981a8c35c06d7b52bbe0804c
luclagarde <75084549+luclagarde@users.noreply.github.com>
2021-06-10 14:12:45 -0400

    Avec commentaire

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
-basic.forever(function () {
-	
+/**
+ * Voici quelques suggestions:
+ * 
+ * Tu peux modifier ce code pour partir d'une valeur fixe et faire diminuer le décompte.
+ * 
+ * Ajoute un beep à chaque fois que ton nombre change.
+ */
+input.onButtonPressed(Button.A, function () {
+    nombre += 1
+    basic.showNumber(nombre)
 })
+input.onButtonPressed(Button.B, function () {
+    nombre = 0
+    basic.showNumber(nombre)
+})
+let nombre = 0
+basic.showNumber(0)
+nombre = 0
